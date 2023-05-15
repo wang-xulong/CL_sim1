58bc165393d1b7ac695cd2299acec2753d404ac7
wang-xulong <wangxl05292016@gmail.com>
2023-05-15 17:16:32 +0100

    change v2 dataset

--- run3.1.py
+++ run3.1.py
@@ -23,7 +23,7 @@ config = Namespace(
     test_bs=128,
     lr_init=0.001,
     max_epoch=500,
-    run_times=10,
+    run_times=2,
     patience=20
 )
 loss_num = 7
--- util.py
+++ util.py
@@ -216,8 +216,8 @@ def get_Cifar10(train_bs=128, test_bs=128):
 
 def get_Cifar100(train_bs=128, test_bs=128):
     base_dir = "/home/acq21xw"
-    train_dir = os.path.join(base_dir, "Data", "SplitCifar100_2class_v1", "train")
-    test_dir = os.path.join(base_dir, "Data", "SplitCifar100_2class_v1", "test")
+    train_dir = os.path.join(base_dir, "Data", "SplitCifar100_2class_v2", "train")
+    test_dir = os.path.join(base_dir, "Data", "SplitCifar100_2class_v2", "test")
     train_stream = []
     test_stream = []
     # MNIST 数据集处理
